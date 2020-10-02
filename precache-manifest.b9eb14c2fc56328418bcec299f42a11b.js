8df6097ce42abbd9d3dc74603ffe82c5d58d6a7d
akcode101 <akale2009@gmail.com>
2020-10-01 22:44:40 -0700

    Updates

---
+++ precache-manifest.b9eb14c2fc56328418bcec299f42a11b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdeb49407c37722364b12d124cc48824",
+    "url": "/Shopping-Cart/index.html"
+  },
+  {
+    "revision": "58752d5f9486002ff60a",
+    "url": "/Shopping-Cart/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "e7270644e89a65f5f2b4",
+    "url": "/Shopping-Cart/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "58752d5f9486002ff60a",
+    "url": "/Shopping-Cart/static/js/2.5b44faae.chunk.js"
+  },
+  {
+    "revision": "e7270644e89a65f5f2b4",
+    "url": "/Shopping-Cart/static/js/main.2c8a9b3c.chunk.js"
+  },
+  {
+    "revision": "fefe738030062c2c7754",
+    "url": "/Shopping-Cart/static/js/runtime~main.9ff5cdc7.js"
+  }
+]);
